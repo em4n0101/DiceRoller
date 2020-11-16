857466a3f150f2d856c650f182a13e97a4fe7195
Emmanuel Cruz <em4n0101@gmail.com>
2020-11-15 22:05:49 -0600

    (code): Basic roll dice implementation

--- app/src/main/java/com/em4n0101/diceroller/MainActivity.kt
+++ app/src/main/java/com/em4n0101/diceroller/MainActivity.kt
@@ -2,10 +2,22 @@ package com.em4n0101.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    rollDice()
+
+    rollButton.setOnClickListener {
+       rollDice()
+    }
   }
+
+  private fun rollDice() {
+    randomNumberTextView.text = (1..6).random().toString()
+  }
+
+
 }
